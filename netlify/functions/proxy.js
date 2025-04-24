65a425c9ccc7cab450836bb94c11b87cfc8720d4
dinzz5467 <86393160+dinzz5467@users.noreply.github.com>
2025-04-24 10:04:34 +0800

    Create proxy.js

---
+++ netlify/functions/proxy.js
@@ -0,0 +1,32 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function(event) {
+  const url = new URL(event.rawUrl);
+  const target = url.searchParams.get("url");
+
+  if (!target) {
+    return {
+      statusCode: 400,
+      body: "Missing 'url' query parameter"
+    };
+  }
+
+  try {
+    const response = await fetch(target);
+    const body = await response.text();
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Content-Type": "application/json"
+      },
+      body: body
+    };
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: `Error: ${err.toString()}`
+    };
+  }
+};
